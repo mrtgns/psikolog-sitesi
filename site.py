35c27c4565e2a122d68467b6563fcfeb18c755c6
Murat Güneş <mgunes004@gmail.com>
2020-12-28 13:53:50 +0300

    ilk commit

---
+++ site.py
@@ -0,0 +1,251 @@
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.handlers.sha2_crypt import sha256_crypt
+from functools import wraps
+import psycopg2
+from datetime import datetime
+
+#Kullanıcı Giriş Decaratoru
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:
+            flash("Bu sayfayı görüntülemek için giriş yapınız..","danger")
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+
+
+app=Flask(__name__)
+app.secret_key = 'show_message' 
+
+DB_URL = 'postgresql://postgres:murat123@localhost/et_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+class LoginForm(Form):
+    username=StringField("kullanıcı Adı")
+    password=PasswordField("Parola")
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/article")
+def article():
+    articles=Articles.query.all()
+    #conn = psycopg2.connect(DB_URL)
+    #cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    #query = 'SELECT * FROM public.articles'
+    #result = cursor.execute(query)
+    #articles= cursor.fetchall()
+
+    #cursor.close()
+    #conn.close()
+
+    if articles:
+        return render_template('article.html', articles=articles)
+
+    return render_template("article.html")
+
+#Yazı ekleme
+@app.route("/addarticle",methods=["POST","GET"])
+@login_required
+def addarticle():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        content = form.content.data
+        newArticle = Articles(title=title, content=content)
+        
+        
+        db.session.add(newArticle)
+        db.session.commit()
+        
+        return redirect(url_for('dashboard'))
+
+
+        #title = form.title.data
+        #content = form.content.data
+
+        #conn = psycopg2.connect(DB_URL)
+        #cursor = conn.cursor()
+        #query = 'insert into public.articles(title,author,content) VALUES(%s,%s,%s)'
+        #cursor.execute(query, (title, session['username'], content))
+        #conn.commit()
+        #cursor.close()
+        #conn.close()
+        #flash('Yazınız başarılı bir şekilde eklendi', 'success')
+        #return redirect(url_for('dashboard'))
+
+    return render_template("addarticle.html",form=form)
+
+#detay sayfası
+@app.route("/articles/<string:id>")
+def articles(id):
+    article=Articles.query.all()
+   
+    if article:
+         article=Articles.query.filter_by(id=id).first()
+         return render_template("articles.html",article=article)
+    else:
+        return render_template("articles.html")
+
+#yazı silme
+@app.route("/delete/<string:id>")
+@login_required
+def delete(id):
+    article=Articles.query.all()
+   
+    if article:
+   
+        article=Articles.query.filter_by(id=id).first()
+        db.session.delete(article)
+        db.session.commit()
+        return redirect(url_for("dashboard"))
+        
+    return render_template("dashboard.html",article=article)
+#yazı güncelle
+@app.route("/edit/<string:id>",methods=["GET","POST"])
+@login_required
+def update(id):
+    Articles.query.all()
+    if request.method=="GET" :
+       
+        article=Articles.query.filter_by(id=id).first()
+
+        form=ArticleForm()
+        form.title.data=article.title
+        form.content.data=article.content
+       
+        return render_template("update.html",form=form)
+
+    else:
+        article=Articles.query.filter_by(id=id).first()
+        form=ArticleForm(request.form)
+        article.title=form.title.data
+        article.content=form.content.data
+        
+        db.session.commit()
+        flash("Yazınız güncenlendi...","success")
+        return redirect(url_for("dashboard"))
+        
+
+
+#Yazı Form sayfası
+class ArticleForm(Form):
+    title = StringField('Yazı Başlığı', [
+        validators.DataRequired(message='required'),
+        validators.Length(min=5, max=300)
+    ])
+    content = TextAreaField('Yazı içeriği', [
+        validators.InputRequired(message='required'),
+        validators.Length(min=10)
+    ])
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    
+    articles=Articles.query.all() 
+    
+    if articles:
+        return render_template('dashboard.html', articles=articles)
+
+    else:
+        return render_template("dashboard.html")
+
+    
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    form=LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        usernamex = form.username.data
+        passwordenter = form.password.data
+        User.query.all()
+        if User.query.filter_by(username=usernamex,password=passwordenter).first():
+
+            session['logged_in'] = True
+            session['username']=usernamex
+            flash('Başarı ile giriş yaptınız', 'success')
+                
+            return redirect(url_for('article'))
+
+
+        
+        else:
+            flash('Kullanıcı adı veya parola yanlış', 'danger')
+            return redirect(url_for('login'))
+
+        #conn = psycopg2.connect(DB_URL)
+        #cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        #query = 'SELECT * FROM public.user WHERE username = %s'
+        #result = cursor.execute(query, (username,))
+        #record = cursor.fetchone()
+        #cursor.close()
+        #conn.close()
+        #if(record):
+            #realPassword = record['password']
+            #if realPassword==password:
+                #session['logged_in'] = True
+                #session['username'] = record['username']
+                #session['user_id'] = record['id']
+                #flash('basari ile giris yaptiniz', 'success')
+                #return redirect(url_for('article'))
+            #else:
+                #flash('sifre yanlis', 'danger')
+                #return redirect(url_for('login'))
+        #else:
+            #flash('kullanici adi yanlis', 'danger')
+            #return redirect(url_for('login'))
+
+    else:
+        return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    #created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.now())
+db.create_all()
+
+class Articles(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   title = db.Column(db.String(100), nullable=False) # (nullable=False) == (NOT NULL)
+   content= db.Column(db.String() , nullable=False)
+   created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.now())
+db.create_all()
+
+if __name__=="__main__":
+    
+    app.run(debug=True)
+   
